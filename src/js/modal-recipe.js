3da72c2a4b36b5e2bfbee517cfe0452d640845c9
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-07-25 20:08:14 +0300

    css

--- src/js/modal-recipe.js
+++ src/js/modal-recipe.js
@@ -17,16 +17,29 @@ function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
 }
 
+async function loadRecipeId() {
+  let requestParams = `/recipes/${id}`;
+  const { data } = await unsplashApi.fetchRecipes(requestParams);
+  console.log(data);
+  createRecipesCards(data);
+}
+
+loadRecipeId();
+
 async function heardleRecipeById() {}
 
 function markup(data) {
-  const ingredients = data.map(({ ingredients }) => {
-    return `<li><span class="ingredients-name">${ingredients.name}</span><span class="ingredients-measure">${ingredients.measure}</span></li>`;
-  });
+  const ingredients = data
+    .map(({ ingredients }) => {
+      return `<li><span class="ingredients-name">${ingredients.name}</span><span class="ingredients-measure">${ingredients.measure}</span></li>`;
+    })
+    .join('');
 
-  const tags = data.map(({ tags }) => {
-    return `<li>${tags}</li>`;
-  });
+  const tags = data
+    .map(({ tags }) => {
+      return `<li>${tags}</li>`;
+    })
+    .join('');
 
   return data
     .map(({ title, time, instructions, youtube, rating }) => {
--- src/js/recipes-list.js
+++ src/js/recipes-list.js
@@ -20,7 +20,7 @@ function createRecipesCards (data) {
                 <img src="${recipe.preview}" alt="${recipe.description}" loading="lazy" />
                 <p class="recipe-card-title">${recipe.title}</p>
                 <p class="recipe-card-description">${recipe.description}</p>
-                <button type="button" class="recipe-card-button">See recipe</button>
+                <button type="button" class="recipe-card-button data-modal-recipte-open">See recipe</button>
               </div>
             </li>`;
             recipesListEl.insertAdjacentHTML("beforeend", markup);
