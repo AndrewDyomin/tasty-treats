5c1b88e453282b3e3ec2a04acf90bdf8510ee005
AndrewDyomin <dyomin.andrew1@gmail.com>
2023-07-26 01:23:02 +0300

    update api filter recipes.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
 export class UnsplashAPI {
-    
-#BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
 
-async fetchRecipes(requestParams) {
-  return await axios.get(`${this.#BASE_URL}${requestParams}`);
+currentPage = 1;
+itemsPerPage = 6;    
+#BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+endpoint = '';
+
+async fetchRecipes() {
+  return await axios.get(`${this.#BASE_URL}${this.endpoint}`, {
+    params: {
+      page: this.currentPage,
+      limit: this.itemsPerPage,
+      title: this.searchQuerry,
+      time: this.queryTime,
+      area: this.queryArea,
+      ingredient: this.queryIngredient,
+    },
+  });
   }
 }
-
