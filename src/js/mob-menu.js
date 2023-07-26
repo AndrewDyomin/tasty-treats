77d7d76892fce95f8200904f68826a4322c1f1e4
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-07-26 21:04:11 +0300

    Update mob-menu.js

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,10 +2,11 @@ const mobMenuBtn = document.querySelector('.mob-menu-btn');
 const mobMenuCloseBtn = document.querySelector('.mob-menu-btn-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-mobMenuBtn.addEventListener('click', toggleOpen);
-
-mobMenuCloseBtn.addEventListener('click', toggleOpen);
-
-function toggleOpen(){
+mobMenuBtn.addEventListener('click', function() {
   mobileMenu.classList.toggle('open');
-}
+});
+
+mobMenuCloseBtn.addEventListener('click', function() {
+  mobileMenu.classList.remove('open');
+});
+
