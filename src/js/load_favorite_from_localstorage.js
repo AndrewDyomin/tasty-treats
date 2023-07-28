771c9520f766e77944cb55cf9041d78aae4950e1
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-07-28 18:37:35 +0300

    delete local favorite items

--- src/js/load_favorite_from_localstorage.js
+++ src/js/load_favorite_from_localstorage.js
@@ -6,6 +6,8 @@ const unsplashApi = new UnsplashAPI;
 const dataFromLocalStorage = localStorage.getItem("listOfFavoriteRecipe")
 const listOfFavItems = JSON.parse(dataFromLocalStorage);
 const favList = document.querySelector('.fav-resipes-list');
+const newDataStorage = {};
+const LOCALSTORAGE_KEY = "listOfFavoriteRecipe";
 
 function download() {
     for (let item in listOfFavItems) {
@@ -23,10 +25,10 @@ async function reloadRecipesList (item) {
 function createRecipesCards (data) {
     try {
             const markup = 
-            `<li class="fav-recipes-list-item"">
+            `<li class="fav-recipes-list-item" id="${data._id}">
             <div class="recipe-card-gradient"></div>
-                <svg class="favorite-icon">
-                  <use id="iconUse" href="${svg}#icon-heart"></use>
+                <svg class="favorite-icon" >
+                  <use id="iconUse" fill="#f8f8f8"; href="${svg}#icon-heart"></use>
                 </svg>
               <div class="fav-recipe-card">
                 <img src="${data.preview}" alt="${data.description}" loading="lazy" />
@@ -39,4 +41,23 @@ function createRecipesCards (data) {
     } catch (err) {
         Notiflix.Notify.warning('Sorry, something went wrong. Please try later.');
     }
+}
+
+favList.addEventListener('click', removeFavRecipe);
+
+function removeFavRecipe(e) {
+  const iconBtn = e.target;
+  if (iconBtn.id !== 'iconUse') {
+        return;
+    }
+  const recipeId = e.target.closest("li").id;
+  for (let item in listOfFavItems) {
+    if (item !== recipeId) {
+      newDataStorage[item] = item;
+    }
+  }
+  iconBtn.style.fill = "none";
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newDataStorage));
+  location.reload();
+  
 }
