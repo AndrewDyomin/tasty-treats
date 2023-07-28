351a62a7a0f51debec7e8ff001274b9a038fd0f0
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-07-28 13:48:19 +0300

    add recipe to localStorage

--- src/js/add_favorite_to-localstorage.js
+++ src/js/add_favorite_to-localstorage.js
@@ -1,27 +1,31 @@
-
 const refs = {
     recipeList: document.querySelector('.resipes-list'),
     LOCALSTORAGE_KEY: "listOfFavoriteRecipe",
 }
 
-const listOfFavoriteRecipe = {};
+const data = {};
 
 refs.recipeList.addEventListener('click', clickOnSvg);
+
+
 
 function clickOnSvg(e) {
     const btnSvg = e.target;
     if (btnSvg.id !== 'iconUse') {
         return;
     }
-    console.log('Done!');
-    console.log(btnSvg.closest("li"))
+    console.log('Done!', btnSvg);
+    console.log(btnSvg.closest("li").id);
+    data[btnSvg.closest("li").id] = btnSvg.closest("li").id;
+    addDataToLocalStorage();
     if (btnSvg.style.fill !== "rgb(248, 248, 248)") {
         return btnSvg.style.fill = "#f8f8f8";
+        
     } else {
         return btnSvg.style.fill = "none";
     }
 }
 
 function addDataToLocalStorage() {
-    localStorage.setItem(refs.LOCALSTORAGE_KEY, JSON.stringify(refs.LOCALSTORAGE_KEY));
+    localStorage.setItem(refs.LOCALSTORAGE_KEY, JSON.stringify(data));
 }
--- src/js/recipes-list.js
+++ src/js/recipes-list.js
@@ -27,8 +27,8 @@ function createRecipesCards (data) {
         recipesListEl.innerHTML = '';
         for (const recipe of data.results) {
             const markup = 
-            `<li class="recipes-list-item">        
-                <svg class="favorite-icon" name="${recipe._id}">
+            `<li class="recipes-list-item" id="${recipe._id}">        
+                <svg class="favorite-icon">
                   <use id="iconUse" href="${svg}#icon-heart"></use>
                 </svg>
               <div class="recipe-card">
