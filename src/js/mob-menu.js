bf93612e844d86148e170ad9cf9ec7d0cd857cff
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-07-28 19:33:19 +0200

    corrected class no-scroll

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,12 +1,15 @@
 const mobMenuBtn = document.querySelector('.mob-menu-btn');
 const mobMenuCloseBtn = document.querySelector('.mob-menu-btn-close');
 const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.body;
 
 mobMenuBtn.addEventListener('click', function() {
   mobileMenu.classList.toggle('open');
+  body.classList.add('no-scroll');
 });
 
 mobMenuCloseBtn.addEventListener('click', function() {
   mobileMenu.classList.remove('open');
+  body.classList.remove('no-scroll');
 });
 
